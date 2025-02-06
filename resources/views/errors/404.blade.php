2838727f813e38af160fb8c217af14bb349ffef9
kroos <dhiauddin@gmail.com>
2025-02-07 02:27:00 +0800

    -- fix errors/401.blade.php errors/404.blade.php errors/419.blade.php errors/500.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
 
-	<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-		{{ __('Error 404') }}
-	</h2>
-	<div class="col-12-sm" >
+	<div class="col-sm-8 text-center tw">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+			{{ __('Error 404 : Page Not Found') }}
+		</h2>
+	</div>
+
+	<div class="col-sm-8 flex justify-content-center" >
 		<a href="{{ url('/dashboard') }}" class="">
 			<img src="{{ asset('images/errors/404-error.jpg') }}" class="img-fluid rounded " alt="">
 		</a>
